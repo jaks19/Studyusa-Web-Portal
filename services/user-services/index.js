64e8f6bcf194de3531361f4161b6bad2bc9312ab
jaks19 <jaks19@mit.edu>
2017-07-14 23:24:07 +0000

    refactor user services and fix admin dashboard bug

--- services/user-services/index.js
+++ services/user-services/index.js
@@ -3,19 +3,18 @@ let User = require("../../models/user"),
     notifServices = require('../notif-services'),
     dbOpsServices = require('../dbops-services'),
     apiServices = require('../api-services');
-    
+
 userServices.getUserData = async function getUserData(username, req, res) {
     var userData = {};
-    userData.populatedUser = await dbOpsServices.findOneEntryAndPopulate(User, {'username': username}, ['submissions', 'notifs'], req, res)
+    userData.populatedUser = await dbOpsServices.findOneEntryAndPopulate(User, {'username': username}, ['submissions', 'notifs'], req, res),
     userData.allNotifs = userData.populatedUser.notifs.reverse(),
     userData.unseenNotifs = notifServices.getUnseenNotifs(userData.populatedUser.notifs),
     userData.firstContact = req.query.welcome ? true : false;
-    
     if (userData.populatedUser.admin){
-        userData.users = await userServices.findAllUsers(req);
+        userData.users = await dbOpsServices.findAllEntriesAndPopulate(User, { }, [ ], req, res);
     } else {
         userData.articles = await apiServices.retrieveNews(req),
-        userData.subs = userData.populatedUser.submissions.reverse()
+        userData.subs = userData.populatedUser.submissions.reverse();
     }
     return userData;
 }
