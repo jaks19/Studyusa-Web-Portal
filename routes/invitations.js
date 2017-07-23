4e744069c2a89dd9165a887c054b3bbdc1eadcd4
jaks19 <jaks19@mit.edu>
2017-07-23 03:36:56 +0000

    add UI for invitation code handling on admin dashboard

--- routes/invitations.js
+++ routes/invitations.js
@@ -12,15 +12,13 @@ const validDays  = 7,
 // New Invitation
 router.post('/new', authServices.isAdmin, async function(req, res) {
     let newCode = invitationServices.generateCode(codeLength),
-        nickname = req.query['nickname'],
+        nickname = req.body.nickname,
         dateNow = new Date(Date.now()),
         dateValid = (new Date()).setDate(dateNow.getDate() + validDays),
         newInvitationData = new Invitation({ code: newCode, validUntil: dateValid, nickname: nickname }),
         newInvitation = await dbopsServices.createEntryAndSave(Invitation, newInvitationData, req, res);
     invitationServices.garbageCollectInvitations(req, res, garbageCollectDays);
-    console.log('new', newInvitation);
-    req.flash('success', `Created code: ${newCode} for ${nickname}`)
-    res.redirect('back');
+    res.redirect('/index/admin/?invitation=yes');
 });
 
 module.exports = router;
