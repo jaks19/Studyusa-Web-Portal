8973ee86ab446bbe4f9dbb07bdd67d7846e07345
Adarsh <jaks19@mit.edu>
2018-08-28 18:18:58 -0400

    Index user schema on username and make username need to be unique

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose"),
 
 var userSchema = new mongoose.Schema({
     name: String,
-    username: String,
+    username: {type: String, index: true, unique: true},
     password: String,
     admin: {type: Boolean, default: false},
     dateJoined: {type: Date, default: Date.now},
@@ -21,5 +21,10 @@ var userSchema = new mongoose.Schema({
 });
 
 userSchema.plugin(passportLocalMongoose);
+
+// Index the db by the username because search by username often
+// Gives search as fast as by _id since index is on _id by default
+// (Both _id and username will be fast, this does not override)
+// (Done in the schema itself, see username field)
 
 module.exports = mongoose.model("User", userSchema);
